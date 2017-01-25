be4f5fcd9cdcd1dbfc5033d3f2036e804053b241
Akshay Jain <jain.akshay5244@gmail.com>
2017-01-25 20:04:19 +0530

    .

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ app.get('/webhook', function (req, res) {
 
 // handler receiving messages
 app.post('/webhook', function (req, res) {
+  getStartMessage();
   welcomeMessage();
   var events = req.body.entry[0].messaging;
   for (i = 0; i < events.length; i++) {
